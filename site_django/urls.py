0cc8eb24b69bc4ed88a051d07a3fe6337ff83084
maksim-shmat <maksim.shmat@gmail.com>
2020-10-27 18:24:36 +0300

    Add Xml map, and test stash + rebase(fail) + stash pop

--- site_django/urls.py
+++ site_django/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from notes.sitemaps import PostSitemap
 
+sitemaps = {'posts': PostSitemap,}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notes/', include('notes.urls', namespace='notes')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+          name='django.contrib.sitemaps.views.sitemap')
 ]
 
